14929e33278cb00e246061c14278fc91b65276db
Kenneth Tran <kennethtran93@gmail.com>
2016-01-07 15:10:26 -0800

    Result from lab instruction:  PHP Functions
    First Draft of the PHP Tic Tac Toe Game.

--- index.php
+++ index.php
@@ -12,21 +12,78 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        $name = 'Ken';
-        $what = 'geek';
-        $level = 10;
-        echo 'Hi, my name is ' . $name . ', and I am a level ' . $level . ' ' . $what;
-        
-        // From PHP Expressions and PHP Selection
-        $hoursworked = $_GET['hours'];
-        $rate = 12;
-        if ($hoursworked > 40) {
-            $total = $hoursworked * $rate * 1.5;
+        if (isset($_GET['board'])) {
+            // board variable found
+            $position = $_GET['board'];
+            if (strlen($position) == 9) {
+                // Perfect variable response.  Continuing...
+                $squares = str_split($position);
+                if (winner('x', $squares)) {
+                    echo 'X is the winner in this game.';
+                } else if (winner('o', $squares)) {
+                    echo 'O is the winner in this game.';
+                } else {
+                    echo 'No winner yet.';
+                }
+            } else {
+                // Imperfect variable response.
+                echo 'Invalid variable response.  Ensure the variable contains exactly nine characters.<br />';
+                echo 'Use x and o for the respective players.  Use - (dash) for an empty square.';
+                
+            }
         } else {
-            $total = $hoursworked * $rate;
+            // board variable not found.
+            echo 'PHP GET variable "board" not found. <br />';
+            echo 'Please append the following to the URL above: <br /><br />';
+            echo '?board=--------- <br /><br />';
+            echo 'where each individual dash is a place in the tic tac toe board, going left to right, up to down.';
         }
-        echo '<br />';
-        echo ($total > 0) ? 'You owe me $' . $total : "You're welcome.";
         ?>
     </body>
 </html>
+
+<?php
+
+function winner($token, $position) {
+    $won = false;
+    // Row Checking
+    if (($position[0] == $token) &&
+            ($position[1] == $token) &&
+            ($position[2] == $token)) {
+        $won = true;
+    } else if (($position[3] == $token) &&
+            ($position[4] == $token) &&
+            ($position[5] == $token)) {
+        $won = true;
+    } else if (($position[6] == $token) &&
+            ($position[7] == $token) &&
+            ($position[8] == $token)) {
+        $won = true;
+    }
+    // Column Checking
+    else if (($position[0] == $token) &&
+            ($position[3] == $token) &&
+            ($position[6] == $token)) {
+        $won = true;
+    } else if (($position[1] == $token) &&
+            ($position[4] == $token) &&
+            ($position[7] == $token)) {
+        $won = true;
+    } else if (($position[2] == $token) &&
+            ($position[5] == $token) &&
+            ($position[8] == $token)) {
+        $won = true;
+    }
+    // Diagonal Checking
+    else if (($position[0] == $token) &&
+            ($position[4] == $token) &&
+            ($position[8] == $token)) {
+        $won = true;
+    } else if (($position[2] == $token) &&
+            ($position[4] == $token) &&
+            ($position[6] == $token)) {
+        $won = true;
+    }
+    return $won;
+}
+?>
