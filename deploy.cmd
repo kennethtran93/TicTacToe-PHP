a22945e98c426ff483db6c39dacb7e37714ebe65
Kenneth Tran <kennethtran93@gmail.com>
2016-01-08 01:02:17 -0800

    Attempt #5b at Azure Custom Deployment removed exclude parameter.  Simply putting one file in its place now.

--- deploy.cmd
+++ deploy.cmd
@@ -2,4 +2,4 @@
 echo ---Deploying site
 
 REM ---Deploy the wwwroot folder in repository to custom target (wwwroot\lab01)
-robocopy %DEPLOYMENT_SOURCE% %DEPLOYMENT_TARGET%\lab01 *.* /XF .deployment .gitignore .md .cmd /IS /S
+robocopy %DEPLOYMENT_SOURCE% %DEPLOYMENT_TARGET%\lab01 index.php /IS /S
