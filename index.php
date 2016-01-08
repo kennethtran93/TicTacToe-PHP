a270afdd4fc0032a295cb9ebed66928662ba261c
Kenneth Tran <kennethtran93@gmail.com>
2016-01-08 02:20:24 -0800

    Result from lab instruction: Gameplay - Interactivity Now playing against AI. AI features: --If win move for self available, take it. --If win move for player available, try to block it. --If no win move foreseeable, pick randomly. Other edits: Visual Appearance of Tic Tac Toe Grid. Wider area to click on cell/link. Added Tie Game Comments. When game is over the game board links disappear.

--- index.php
+++ index.php
@@ -21,14 +21,27 @@ and open the template in the editor.
                 // Start Game Board
                 $game = new Game($position);
 
-                if ($game->winner('x')) {
-                    echo '<strong>X is the winner in this game.</strong>';
+                if ($position == '---------') {
+                    echo 'A new game as started.  Since I am the game host, I\'ll let you start first.<br />';
+                    echo 'Click on a dash to mark your territory with an X!';
+                } else if (substr_count($position, 'x') - substr_count($position, 'o') > 1) {
+                    echo 'ERROR DETECTED - There are too many Xs on the game board.  QUIT CHEATING!<br />';
+                    echo '<a href="' . $_SERVER['PHP_SELF'] . '">Click here to start a new game!</a>';
+                } else if (substr_count($position, 'o') - substr_count($position, 'x') > 0) {
+                    echo 'ERROR DETECTED - There are too many Os on the game board.  Since I\'m not programmed to cheat, you must have done something to the game board.<br />';
+                    echo '<a href="' . $_SERVER['PHP_SELF'] . '">Click here to start a new game!</a>';
+                } else if ($game->winner('x')) {
+                    echo '<strong>X (YOU) is the winner in this game.  Congratulations.</strong><br />';
+                    echo '<a href="' . $_SERVER['PHP_SELF'] . '">Click here to start a new game!</a>';
                 } else if ($game->winner('o')) {
-                    echo '<strong>O is the winner in this game.</strong>';
-                } else if ($position == '---------') {
-                    echo 'A new game as started.  Click on a dash to mark your territory with an X!';
+                    echo '<strong>O (Ken\'s AI) is the winner in this game.  YOU FAIL!</strong><br />';
+                    echo '<a href="' . $_SERVER['PHP_SELF'] . '">Click here to start a new game!</a>';
+                } else if (stristr($position, '-') === FALSE) {
+                    echo 'This game is TIED as there are no more moves left.<br />';
+                    echo '<a href="' . $_SERVER['PHP_SELF'] . '">Click here to start a new game!</a>';
                 } else {
-                    echo '<strong>No winner yet.</strong>';
+                    echo 'No winner yet.  <strong>It\'s your turn now.</strong><br />';
+                    echo 'Alternatively you can chicken out, which means that the host wins by default, by <a href="' . $_SERVER['PHP_SELF'] . '">clicking here to start a new game!</a>';
                 }
             } else if (strlen($position) == 0) {
                 // variable exist, but empty
@@ -39,7 +52,7 @@ and open the template in the editor.
                 echo 'Invalid game board.  Ensure the variable contains exactly nine (9) characters.<br />';
                 echo 'There are currently ' . strlen($position) . ' characters on the game board.<br />';
                 echo 'Either fix the game board variable in the URL, click the browser back button,'
-                . ' or <a href="?board=---------">click here to start a new game</a>.';
+                . ' or <a href="' . $_SERVER['PHP_SELF'] . '">click here to start a new game</a>.';
             }
         } else {
             // board variable not found.  Creating one now...
@@ -66,15 +79,27 @@ class Game {
             // Debug variable declared
             $this->DEBUG = true;
         }
-        if ($this->winner('x') || $this->winner('o')) {
+        if ((substr_count($this->board, 'x') - substr_count($this->board, 'o')) > 1 || (substr_count($this->board, 'o') - substr_count($this->board, 'x')) > 0) {
             $this->gameOver();
         } else {
-            $this->displayGrid();
+            if ($this->winner('x') || $this->winner('o') || stristr($this->board, '-') === FALSE) {
+                $this->gameOver();
+            } else if ($this->board == '---------') {
+                $this->displayGrid();
+            } else {
+                $this->pick_move();
+                if ($this->winner('o')) {
+                    $this->gameOver();
+                } else {
+                    $this->displayGrid();
+                }
+            }
         }
     }
 
     function displayGrid() {
-        echo '<table cols="3" style="font-size:large; font-weight:bold">'; // starts table
+        echo '<font face="courier" size="5">';
+        echo '<table cols="3" border="1" style="font-weight:bold; border-collapse: collapse">'; // starts table
         echo '<tr>'; // opens the first row
         for ($pos = 0; $pos < 9; $pos++) {
             echo $this->show_cell($pos);
@@ -84,22 +109,103 @@ class Game {
         }
         echo '</tr>'; // closes the last row
         echo '</table>'; // closes table
+        echo '</font>';
         echo '<hr />';  // separates the game board from results
     }
 
     function show_cell($which) {
         $token = $this->position[$which];
-// deal with the easy case
+        // deal with the easy case
         if ($token <> '-') {
-            return '<td>' . $token . '</td>';
+            return '<td style="padding: 5px;">' . $token . '</td>';
         }
-// now the card case
+        // now the card case
         $this->newposition = $this->position; // copy original
         $this->newposition[$which] = 'x'; // this would be their move
         $move = implode($this->newposition); // make a string from board array
         $link = '?board=' . $move; // this is what we want the link to be
-// so return cell containing an anchor and showing a hyphen
-        return '<td><a href="' . $link . '">-</a></td>';
+        // so return cell containing an anchor and showing a hyphen
+        return '<td><a href="' . $link . '" style="text-decoration: none;"><div style="padding: 5px;">-</div></a></td>';
+    }
+
+    function pick_move() {
+        // Let's check if there's a winning move
+        $ai_win_move = $this->ai_check_move('o');
+        if ($ai_win_move != -1) {
+            $this->position[$ai_win_move] = 'o';
+        } else {
+            $player_win_move = $this->ai_check_move('x');
+            if ($player_win_move != -1) {
+                $this->position[$player_win_move] = 'o';
+            } else {
+                $board = implode($this->position);
+                $move = 0;
+                do {
+                    $move = rand(0, 8);
+                } while (substr($board, $move, 1) != '-');
+                $new_board = substr_replace($board, 'o', $move, 1);
+                $this->position = str_split($new_board);
+            }
+        }
+    }
+
+    function ai_check_move($token) {
+        // Check board for potential winning moves for token.
+        // Horizontal checking
+        for ($row = 0; $row < 3; $row++) {
+            $check_value = 0;
+            $win_move = 0;
+            for ($col = 0; $col < 3; $col++) {
+                if ($this->position[3 * $row + $col] != $token) {
+                    $win_move = 3 * $row + $col;
+                } else {
+                    $check_value++;
+                }
+            }
+            if ($check_value == 2) {
+                if ($this->position[$win_move] == '-') {
+                    return $win_move;
+                }
+            }
+        }
+        // Vertical checking
+        for ($col = 0; $col < 3; $col++) {
+            $check_value = 0;
+            $win_move = 0;
+            for ($row = 0; $row < 3; $row++) {
+                if ($this->position[3 * $row + $col] != $token) {
+                    $win_move = 3 * $row + $col;
+                } else {
+                    $check_value++;
+                }
+            }
+            if ($check_value == 2) {
+                if ($this->position[$win_move] == '-') {
+                    return $win_move;
+                }
+            }
+        }
+        // Diagonal Checking
+        $diagonals = [[0, 4, 8], [2, 4, 6]];
+        foreach ($diagonals as $line) {
+            $check_value = 0;
+            $win_move = 0;
+            foreach ($line as $pos) {
+                if ($this->position[$pos] != $token) {
+                    $win_move = $pos;
+                } else {
+                    $check_value++;
+                }
+            }
+            if ($check_value == 2) {
+                if ($this->position[$win_move] == '-') {
+                    return $win_move;
+                }
+            }
+        }
+
+        // If we have reached this point, then there are no winning moves available for that token.
+        return -1;
     }
 
     function winner($token) {
@@ -176,16 +282,18 @@ class Game {
     }
 
     function gameOver() {
-        echo '<table cols="3" style="font-size:large; font-weight:bold">'; // starts table
+        echo '<font face="courier" size="5">';
+        echo '<table cols="3" border="1" style="font-weight:bold; border-collapse: collapse">'; // starts table
         echo '<tr>'; // opens the first row
         for ($pos = 0; $pos < 9; $pos++) {
-            echo '<td>' . $this->position[$pos] . '</td>'; // display final result.  no links.
+            echo '<td style="padding: 5px;">' . $this->position[$pos] . '</td>'; // display final result.  no links.
             if ($pos % 3 == 2) {
                 echo '</tr><tr>'; // Start new row
             }
         }
         echo '</tr>'; // closes the last row
         echo '</table>'; // closes table
+        echo '</font>';
         echo '<hr />';  // separates the game board from results
     }
 
